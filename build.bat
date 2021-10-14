8efbd97fb4d701c40e676d4dbe3cf5754302baa8
Muhammed Yasinhan Yaşar <sesden19611@gmail.com>
2021-10-15 00:37:01 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-cmake -DCMAKE_GENERATOR_PLATFORM=x64 ..
+cmake -G "Visual Studio 16 2019" -DCMAKE_GENERATOR_PLATFORM=x64 ..
 cmake --build .
 copy "..\lib\engine.dll" ".\Debug\"
 Xcopy /E "..\resource\" ".\Debug\"
